12fabafadef8536743371919b0a44235b0efe722
Eric da Costa <ericdacosta72@gmail.com>
2022-11-06 16:08:16 -0300

    Refatorando

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ function createGame(player1, hour, player2) {
   `
 }
 
+let delay = -0.3
 function createCard(date, day, games) {
+  delay += 0.3
   return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
       <h2>${date} <span>${day}</span></h2>
       <ul>
         ${games}
@@ -19,18 +21,12 @@ function createCard(date, day, games) {
   `
 }
 
-document.querySelector("#app").innerHTML = `
-  <header>
-    <img src="./assets/logo.svg" alt="" />
-  </header>
-  <main id="cards">
-    ${createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia"))}
-    ${createCard(
-      "28/11",
-      "Segunda",
-      createGame("brazil", "16:00", "switzerland") +
-        createGame("argentina", "09:00", "hungary")
-    )}
-    ${createCard("02/12", "sexta", createGame("japan", "16:00", "cameroon"))}
-  </main>
-`
+document.querySelector("#cards").innerHTML =
+  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+  createCard(
+    "28/11",
+    "Segunda",
+    createGame("brazil", "16:00", "switzerland") +
+      createGame("argentina", "09:00", "hungary")
+  ) +
+  createCard("02/12", "sexta", createGame("japan", "16:00", "cameroon"))
